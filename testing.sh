ef8dfb1279a65b791cf9d194da014ffea6a60432
elenanosti <122377535+elenanosti@users.noreply.github.com>
2025-06-08 18:58:38 +0200

    to run das6

--- testing.sh
+++ testing.sh
@@ -1,7 +1,18 @@
-#!/bin/sh
-#SBATCH --time=1
+#!/bin/bash
+#SBATCH --job-name=ScraperElena
+#SBATCH --time=10:00:00
 #SBATCH -N 1
+#SBATCH --output=congreso_scraper_%j.log
 
-echo “Let’s do some work”
-sleep 20
-echo “All work done.”
+# Load environment
+module load Python/3.13.2  # Adjust to your cluster's Python version
+conda activate      
+
+# Go to your GitHub repo directory
+cd /var/scratch/eei440/Anaconda3/PoliBiasES-BachelorProject-EIEN/
+
+# Make sure your CSV is in place
+cp /var/scratch/eei440/Anaconda3/PoliBiasES-BachelorProject-EIEN/clean_ALL_complete_congreso_links_motion_titel_category_subcat_motionid.csv .
+
+# Run the scraper
+python scraper.py

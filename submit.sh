893dd68b1bd06b90a80e3c1f96f589c1f3328314
elenanosti <122377535+elenanosti@users.noreply.github.com>
2025-06-08 19:16:02 +0200

    Update submit.sh again

--- submit.sh
+++ submit.sh
@@ -4,15 +4,13 @@
 #SBATCH -N 1
 #SBATCH --output=congreso_scraper_%j.log
 
-# Load environment
-module load Python/3.13.2  # Adjust to your cluster's Python version
-conda activate      
+# Activate the base conda environment
+source /var/scratch/eei440/Anaconda3/etc/profile.d/conda.sh
+conda activate base
 
-# Go to your GitHub repo directory
+# Move to your repo folder
 cd /var/scratch/eei440/Anaconda3/PoliBiasES-BachelorProject-EIEN/
 
-# Make sure your CSV is in place
-cp /var/scratch/eei440/Anaconda3/PoliBiasES-BachelorProject-EIEN/clean_ALL_complete_congreso_links_motion_titel_category_subcat_motionid.csv .
-
-# Run the scraper
+# Run the scraper script
 python scraper.py
+

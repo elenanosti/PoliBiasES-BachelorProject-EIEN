171c69dbee9f5aefa6d37445c948770bcbb49614
elenanosti <122377535+elenanosti@users.noreply.github.com>
2025-06-11 13:39:59 +0200

    Trying only one llm

--- runningcode/model_paths.py
+++ runningcode/model_paths.py
@@ -4,10 +4,10 @@ from definitions import (
 
 MODEL_PATHS = {
     "Llama-3-8B-instruct": "meta-llama/Meta-Llama-3-8B-Instruct",
-    "Llama-3-70B-Instruct": "meta-llama/Meta-Llama-3-70B-Instruct",
-    "Mistral-7B-instruct": "mistralai/Mistral-7B-Instruct-v0.1",
-    "Gemma-2-9B-instruct": "google/gemma-2b-it",  # No official 9B; this is a placeholder if needed
-    "Falcon3-7B-instruct": "tiiuae/falcon-7b-instruct",
-    "deepseek-llm-7b-chat": "deepseek-ai/deepseek-llm-7b-chat",
+    # "Llama-3-70B-Instruct": "meta-llama/Meta-Llama-3-70B-Instruct",
+    # "Mistral-7B-instruct": "mistralai/Mistral-7B-Instruct-v0.1",
+    # "Gemma-2-9B-instruct": "google/gemma-2b-it",  # No official 9B; this is a placeholder if needed
+    # "Falcon3-7B-instruct": "tiiuae/falcon-7b-instruct",
+    # "deepseek-llm-7b-chat": "deepseek-ai/deepseek-llm-7b-chat",
 }
 

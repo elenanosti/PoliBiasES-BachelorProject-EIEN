89b7529c10909c1988fb2261253e657e503b0173
elenanosti <122377535+elenanosti@users.noreply.github.com>
2025-06-11 03:58:35 +0200

    corrected model

--- runningcode/model_paths.py
+++ runningcode/model_paths.py
@@ -7,6 +7,6 @@ MODEL_PATHS = {
     # "gemma": "google/gemma-2-9b-it",
     # "falcon": "tiiuae/Falcon3-7B-Instruct",
     # "llama3": "meta-llama/Meta-Llama-3-8B-Instruct",
-    # "deepseek": "deepseek-ai/deepseek-llm-7b-base",
+    # "deepseek": "deepseek-ai/deepseek-llm-7b-chat",
     # "llama70": "meta-llama/Meta-Llama-3-70B-Instruct"
 }

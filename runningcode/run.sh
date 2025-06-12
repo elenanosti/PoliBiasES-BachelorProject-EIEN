88066c188a1065c9fa1dec6f62f749b42877598f
elenanosti <122377535+elenanosti@users.noreply.github.com>
2025-06-12 03:34:06 +0200

    Update run.sh

--- runningcode/run.sh
+++ runningcode/run.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=llm_array
+#SBATCH --job-name=main 
 #SBATCH --account=eei440                
 #SBATCH --nodes=1                                        
-#SBATCH --time=0-06:00:00
-#SBATCH --array=0-3
+#SBATCH --time=0-03:00:00
 
 # === NEW: Set cache directory to scratch space ===
 export HF_HOME=/var/scratch/eei440/hf_cache
 mkdir -p "$HF_HOME"
-# === END: Set cache directory to scratch space ===
+# === END: Set cache directory to scratch space ===E
+
 
 # -------------------- MODULE SETUP --------------------
 module purge
 module use -a /fp/projects01/ec30/software/easybuild/modules/all/
 
-# Uncomment and load your modules as needed
 # module load nlpl-pytorch/2.1.2-foss-2022b-cuda-12.0.0-Python-3.10.8
 # module load nlpl-transformers/4.47.1-foss-2022b-Python-3.10.8
 # module load nlpl-tokenizers/0.21.0-foss-2022b-Python-3.10.8
@@ -24,22 +23,25 @@ module use -a /fp/projects01/ec30/software/easybuild/modules/all/
 # module load JupyterLab/4.0.3-GCCcore-12.2.0
 
 # -------------------- PARAMETERS --------------------
-MODELS=("Llama-3-8B-instruct" "Mistral-7B-instruct" "Gemma-2-9B-instruct" "Falcon3-7B-instruct")
+MODELS=("Falcon3-7B-instruct") # ("Llama-3-8B-instruct", "Llama-3-70B-Instruct" "Mistral-7B-instruct" "Gemma-2-9B-instruct" "Falcon3-7B-instruct" "deepseek-llm-7b-chat")
 PROMPTS=(1)
 PROMPT_TEMPLATES=(0)
+#REPLACES=(2)
 
-MODEL=${MODELS[$SLURM_ARRAY_TASK_ID]}
-
-# -------------------- EXPERIMENT --------------------
-for prompt in "${PROMPTS[@]}"; do
-  for prompt_template in "${PROMPT_TEMPLATES[@]}"; do
-    echo "Running with model=$MODEL, prompt=$prompt, prompt_template=$prompt_template"
-    python3 -u running_experiments.py \
-      --exp=ide \
-      --model="$MODEL" \
-      --prompt="$prompt" \
-      --debug=1 \
-      --datasize=200 \
-      --template="$prompt_template"
+# -------------------- EXPERIMENT LOOP --------------------
+for model in "${MODELS[@]}"; do
+  for prompt in "${PROMPTS[@]}"; do
+    for prompt_template in "${PROMPT_TEMPLATES[@]}"; do
+      #for replace in "${REPLACES[@]}"; do
+        echo "Running with model=$model, prompt=$prompt, prompt_template=$prompt_template" #, replace=$replace"
+        python3 -u running_experiments.py \
+          --exp=ide \
+          --model="$model" \
+          --prompt="$prompt" \
+          --debug=1 \
+          --datasize=200
+          #--replace="$replace" \
+      #done
+    done
   done
 done

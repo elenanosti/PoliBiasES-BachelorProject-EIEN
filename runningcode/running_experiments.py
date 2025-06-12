2365f1b495c3fd6ef7b9a70b451adbd7df8acf63
elenanosti <122377535+elenanosti@users.noreply.github.com>
2025-06-12 18:23:11 +0200

    fully creative high temp

--- runningcode/running_experiments.py
+++ runningcode/running_experiments.py
@@ -325,7 +325,7 @@ def run_experiment(exp_type, model_name, prompt_no=1, cont=0, DEBUG=False, small
                 input_ids,
                 attention_mask=attention_mask,
                 max_new_tokens=max_new_tokens,
-                temperature=.5,
+                temperature=1, # high temperature for more creative output
                 output_scores=True,
                 return_dict_in_generate=True,
                 do_sample=True,

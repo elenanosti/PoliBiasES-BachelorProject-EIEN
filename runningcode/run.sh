d55559cd8d872c1f9150f93f638bff2de9380df6
elenanosti <122377535+elenanosti@users.noreply.github.com>
2025-06-14 23:18:19 +0200

    Update run.sh

--- runningcode/run.sh
+++ runningcode/run.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=main 
 #SBATCH --account=eei440                
 #SBATCH --nodes=1                                        
-#SBATCH --time=0-00:03:00
+#SBATCH --time=0-24:00:00
 #SBATCH --array=0
 
 # === NEW: Set cache directory to scratch space ===

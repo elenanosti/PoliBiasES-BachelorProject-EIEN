8a5fbaa5735ad3ccc731b3b713d5b1b46c82b0d3
elenanosti <122377535+elenanosti@users.noreply.github.com>
2025-06-22 13:13:13 +0200

    Update run.sh

--- runningcode/run.sh
+++ runningcode/run.sh
@@ -21,7 +21,7 @@ module purge
 module use -a /fp/projects01/ec30/software/easybuild/modules/all/
 
 # -------------------- PARAMETERS --------------------
-MODELS=("Gemma-2-9B-instruct") # ("Falcon3-7B-instruct""Llama-2-7b""Aguila-7B-instruct" "Llama-3-70B-Instruct" "Llama-3-8B-instruct"  "Mistral-7B-instruct""deepseek-llm-7b-chat")
+MODELS=("Llama-2-7b") # ("Gemma-2-9B-instruct""Falcon3-7B-instruct""Aguila-7B-instruct" "Llama-3-70B-Instruct" "Llama-3-8B-instruct"  "Mistral-7B-instruct""deepseek-llm-7b-chat")
 
 PROMPTS=(1)
 PROMPT_TEMPLATES=(0)
@@ -38,7 +38,7 @@ for prompt in "${PROMPTS[@]}"; do
     #for replace in "${REPLACES[@]}"; do
       echo "Running with model=$MODEL, prompt=$prompt, prompt_template=$prompt_template" #, replace=$replace"
       python3 -u running_experiments.py \
-        --exp=per \
+        --exp=ide \
         --model="$MODEL" \
         --prompt="$prompt" \
         --debug=1 \
